3c3543b5ac4d07cf4067404ef299712e847c7cff
Maik Schneider <mas@xima.de>
2022-06-13 08:28:46 +0200

    [Feature] Limit output of jobs by TypoScript setting

--- packages/xm_dkfz_net_jobs/Classes/Controller/JobController.php
+++ packages/xm_dkfz_net_jobs/Classes/Controller/JobController.php
@@ -3,6 +3,7 @@
 namespace Xima\XmDkfzNetJobs\Controller;
 
 use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Core\Utility\MathUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use Xima\XmDkfzNetJobs\Utility\JobLoaderUtility;
 
@@ -17,6 +18,19 @@ class JobController extends ActionController
     }
 
     public function latestAction(): ResponseInterface
+    {
+        $jobs = $this->jobLoaderUtility->getJobs();
+
+        if ($this->settings['latestJobCount'] && MathUtility::canBeInterpretedAsInteger($this->settings['latestJobCount'])) {
+            $jobs = array_slice($jobs, 0, (int)$this->settings['latestJobCount']);
+        }
+
+        $this->view->assign('jobs', $jobs);
+
+        return $this->htmlResponse();
+    }
+
+    public function listAction(): ResponseInterface
     {
         $jobs = $this->jobLoaderUtility->getJobs();
 
