99b4640237f20db8cd93c82e3f9631eaf1dcf379
Maik Schneider <mas@xima.de>
2022-06-13 09:21:37 +0200

    [Feature] Register default page and user tsconfig

---
+++ packages/xm_dkfz_net_site/ext_localconf.php
@@ -0,0 +1,5 @@
+<?php
+
+// Register Page- and UserTS config
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('@import "EXT:xm_dkfz_net_site/Configuration/TSconfig/Page.tsconfig"');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('@import "EXT:xm_dkfz_net_site/Configuration/TSconfig/User.tsconfig"');
