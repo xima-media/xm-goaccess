0b730edd0a92c9fb8a7f26cd5a954b9e2c09ce42
Maik Schneider <mas@xima.de>
2022-06-14 15:06:41 +0200

    [Feature] Add deployment configuration for staging

--- deploy.php
+++ deploy.php
@@ -8,3 +8,16 @@ host('local')
     ->hostname('local')
     ->set('deploy_path', getcwd())
     ->set('public_urls', ['https://xm-dkfz-net.ddev.site']);
+
+host('staging')
+    ->hostname('dkfz-typo3-dev.xima.local')
+    ->stage('staging')
+    ->user('xima')
+    ->set('branch', 'master')
+    ->set('public_urls', ['https://dkfz-typo3-dev.xima.local'])
+    ->set('http_user', 'www-data')
+    ->set('writable_mode', 'chmod')
+    ->set('bin/composer', '/usr/local/bin/composer')
+    ->set('bin/php', '/usr/bin/php')
+    ->set('deploy_path', '/var/www/html/dkfz-typo3-dev/typo3-staging');
+
