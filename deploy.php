9f93c9aa891ecd7aff99eff0332d285efc71336d
Maik Schneider <mas@xima.de>
2022-06-15 11:23:02 +0200

    [Feature] Set write permissions for var dir

--- deploy.php
+++ deploy.php
@@ -11,6 +11,16 @@ host('local')
     ->set('deploy_path', getcwd())
     ->set('public_urls', ['https://xm-dkfz-net.ddev.site']);
 
+set('writable_dirs', function () {
+    return [
+        get('web_path') . 'typo3conf',
+        get('web_path') . 'typo3temp',
+        get('web_path') . 'uploads',
+        get('web_path') . 'fileadmin',
+        get('web_path') . '../var',
+    ];
+});
+
 host('staging')
     ->hostname('192.168.2.41')
     ->stage('staging')
@@ -19,6 +29,7 @@ host('staging')
     ->set('public_urls', ['https://dkfz-typo3-dev.xima.local'])
     ->set('http_user', 'www-data')
     ->set('writable_mode', 'chmod')
+    ->set('writable_chmod_mode', '2770')
     ->set('bin/composer', '/usr/local/bin/composer')
     ->set('bin/php', '/usr/bin/php')
     ->set('deploy_path', '/var/www/html/dkfz-typo3-dev/typo3-staging');
