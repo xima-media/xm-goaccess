5e804b350e0967999bf31aa81e75f3281a297ae4
Maik Schneider <mas@xima.de>
2022-06-16 21:11:54 +0200

    [Feature] Remove common fb deployment covered by updated recipes

--- deploy.php
+++ deploy.php
@@ -2,8 +2,6 @@
 
 namespace Deployer;
 
-use Deployer\Exception\GracefulShutdownException;
-
 require_once(__DIR__ . '/vendor/blueways/deployer-recipes/autoload.php');
 
 set('repository', 'git@t3-gitlab-dev.xima.local:dkfz/dkfz-t3-intranet.git');
@@ -12,16 +10,6 @@ host('local')
     ->hostname('local')
     ->set('deploy_path', getcwd())
     ->set('public_urls', ['https://xm-dkfz-net.ddev.site']);
-
-set('writable_dirs', function () {
-    return [
-        get('web_path') . 'typo3conf',
-        get('web_path') . 'typo3temp',
-        get('web_path') . 'uploads',
-        get('web_path') . 'fileadmin',
-        get('web_path') . '../var',
-    ];
-});
 
 host('staging')
     ->hostname('192.168.2.41')
@@ -32,8 +20,6 @@ host('staging')
     ->set('http_user', 'www-data')
     ->set('writable_mode', 'chmod')
     ->set('writable_chmod_mode', '0770')
-    ->set('bin/composer', '/usr/local/bin/composer')
-    ->set('bin/php', '/usr/bin/php')
     ->set('deploy_path', '/var/www/html/stage.dkfz-typo3-dev.xima.local');
 
 host('feature')
@@ -44,8 +30,6 @@ host('feature')
     ->set('http_user', 'www-data')
     ->set('writable_mode', 'chmod')
     ->set('writable_chmod_mode', '0770')
-    ->set('bin/composer', '/usr/local/bin/composer')
-    ->set('bin/php', '/usr/bin/php')
     ->set('public_urls', ['https://fbd.dkfz-typo3-dev.xima.local'])
     ->set('deploy_path', '/var/www/html/fbd.dkfz-typo3-dev.xima.local');
 
@@ -57,83 +41,3 @@ task('deploy:upload-dist', function () {
         '{{release_path}}/packages/xm_dkfz_net_prototype/Resources/Public/'
     );
 });
-
-after('deploy:shared', 'deploy:fix-shared-permissions');
-
-task('deploy:fix-shared-permissions', function () {
-    foreach (get('shared_dirs') ?? [] as $dir) {
-        run('find {{deploy_path}}/shared/' . $dir . ' -type d -exec chmod ' . get('writable_chmod_mode') . ' {} +');
-    }
-});
-
-task('deploy:prepare:feature', function () {
-
-    // abort if feature branch has already been configured
-    if (test('[ -f {{deploy_path}}/.dep/releases.extended ]')) {
-        return;
-    }
-
-    $featureRootPath = get('deploy_path');
-    $branch = get('branch');
-
-    // create shared dir
-    run('mkdir -p {{ deploy_path }}');
-    run('mkdir -p {{ deploy_path }}/shared');
-
-    // look for .env file
-    if (test('[ ! -f ' . $featureRootPath . '/.env ]')) {
-        throw new GracefulShutdownException('No .env file found in "' . $featureRootPath . '/"');
-    }
-
-    // copy .env file
-    run('cp ' . $featureRootPath . '/.env {{deploy_path}}/shared/');
-
-    // set database name
-    run('echo "TYPO3_CONF_VARS__DB__Connections__Default__dbname=\'' . $branch . '\'" >> {{deploy_path}}/shared/.env');
-
-    // set TYPO3_BASE domain
-    foreach (get('public_urls') as $url) {
-        run('echo "TYPO3_BASE=\'' . $url . '\'" >> {{deploy_path}}/shared/.env');
-    }
-
-    // create empty database
-    $dbUser = run('grep TYPO3_CONF_VARS__DB__Connections__Default__user {{deploy_path}}/shared/.env | cut -d "=" -f2 | cut -c2- | rev | cut -c2- | rev');
-    $dbHost = run('grep TYPO3_CONF_VARS__DB__Connections__Default__host {{deploy_path}}/shared/.env | cut -d "=" -f2 | cut -c2- | rev | cut -c2- | rev');
-    $dbPassword = run('grep TYPO3_CONF_VARS__DB__Connections__Default__password {{deploy_path}}/shared/.env | cut -d "=" -f2 | cut -c2- | rev | cut -c2- | rev');
-    $sqlStatement = 'CREATE DATABASE IF NOT EXISTS ' . $branch;
-    run('echo "' . $sqlStatement . '" | mariadb -u ' . $dbUser . ' -h ' . $dbHost . ' --password="' . $dbPassword . '"');
-})->onStage('feature');
-
-task('db:import:feature', function () {
-
-    // abort if feature branch has already been configured
-    if (test('[ -f {{deploy_path}}/.dep/releases.extended ]')) {
-        return;
-    }
-
-    // copy database from source
-    $activePath = get('deploy_path') . '/' . (test('[ -L {{deploy_path}}/release ]') ? 'release' : 'current');
-    run('cd ' . $activePath . ' && {{bin/php}} {{bin/deployer}} db:copy {{ db_source_host }} --options=target:feature');
-})->onStage('feature');
-
-task('override-paths', function () {
-    $featureRootPath = get('deploy_path');
-    $branch = get('branch');
-
-    // override path & public url
-    set('deploy_path', $featureRootPath . '/' . $branch);
-    set('public_urls', array_map(function ($url) use ($branch) {
-        return $url . '/' . $branch . '/current/public';
-    }, get('public_urls')));
-})->onStage('feature');
-
-task('db:truncate', function () {
-})->onStage('feature');
-
-before('db:upload', 'override-paths');
-before('db:import', 'override-paths');
-before('db:rmdump', 'override-paths');
-before('deploy:prepare', 'override-paths');
-before('deploy:extend_log', 'db:import:feature');
-
-before('deploy:prepare', 'deploy:prepare:feature');
