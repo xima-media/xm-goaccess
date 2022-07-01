61b5fb126859da6d654a604b1533baf8d03e6833
Maik Schneider <mas@xima.de>
2022-07-01 10:01:26 +0200

    [Feature] Add video content element

--- packages/xm_dkfz_net_site/Configuration/Icons.php
+++ packages/xm_dkfz_net_site/Configuration/Icons.php
@@ -5,4 +5,8 @@ return [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\FontawesomeIconProvider::class,
         'name' => 'gitlab',
     ],
+    'content-video' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:xm_dkfz_net_site/Resources/Public/Images/icon-content-video.svg',
+    ],
 ];
