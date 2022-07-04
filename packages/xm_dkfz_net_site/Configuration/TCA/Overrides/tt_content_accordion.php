8d76ae77c157d7eb57df6d72eaaa45916d5e9c95
Maik Schneider <mas@xima.de>
2022-07-04 10:27:23 +0200

    [Feature] Add dedicated tt_content fields for accordion items

--- packages/xm_dkfz_net_site/Configuration/TCA/Overrides/tt_content_accordion.php
+++ packages/xm_dkfz_net_site/Configuration/TCA/Overrides/tt_content_accordion.php
@@ -19,9 +19,35 @@
     )->setIcon('content-accordion')
 );
 
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', [
+    'tx_xmdkfznetsite_accordion_title' => [
+        'exclude' => 0,
+        'label' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:container.accordion.title',
+        'description' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:container.accordion.title.description',
+        'config' => [
+            'type' => 'input',
+            'eval' => 'trim,required',
+        ],
+    ],
+    'tx_xmdkfznetsite_accordion_group' => [
+        'exclude' => 0,
+        'label' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:container.accordion.group',
+        'description' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:container.accordion.group.description',
+        'config' => [
+            'type' => 'input',
+        ],
+    ],
+]);
+
+$GLOBALS['TCA']['tt_content']['palettes']['accordion'] = [
+    'label' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:container.accordion.palette',
+    'showitem' => 'tx_xmdkfznetsite_accordion_title,--linebreak--,tx_xmdkfznetsite_accordion_group',
+];
+
 $GLOBALS['TCA']['tt_content']['types']['container-accordion']['showitem'] = '
     --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
         --palette--;;general,
+        --palette--;;accordion,
         --palette--;;headers,
     --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
         --palette--;;frames,
@@ -37,15 +63,3 @@ $GLOBALS['TCA']['tt_content']['types']['container-accordion']['showitem'] = '
         rowDescription,
     --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
 ';
-$GLOBALS['TCA']['tt_content']['types']['container-accordion']['columnsOverrides'] = [
-    'header' => [
-        'description' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:container.accordion.header.description',
-        'config' => [
-            'eval' => 'trim,required',
-        ],
-    ],
-    'subheader' => [
-        'label' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:container.accordion.subheader',
-        'description' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:container.accordion.subheader.description',
-    ],
-];
--- packages/xm_dkfz_net_site/ext_tables.sql
+++ packages/xm_dkfz_net_site/ext_tables.sql
@@ -1,6 +1,8 @@
-#
-# Modifying pages table
-#
 CREATE TABLE pages (
-	tx_xmdkfznetsite_color varchar(64) NOT NULL default ''
+	tx_xmdkfznetsite_color varchar(64) NOT NULL default '',
+);
+
+CREATE TABLE tt_content (
+	tx_xmdkfznetsite_accordion_title varchar(255) NOT NULL default '',
+	tx_xmdkfznetsite_accordion_group varchar(255) NOT NULL default '',
 );
