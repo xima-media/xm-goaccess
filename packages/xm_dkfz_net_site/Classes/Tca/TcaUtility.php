79ea1d33121bb524c669a1766928a70d2f28e414
Maik Schneider <mas@xima.de>
2022-07-04 17:52:12 +0200

    [Feature] Add icons for color selection

--- packages/xm_dkfz_net_site/Classes/Tca/TcaUtility.php
+++ packages/xm_dkfz_net_site/Classes/Tca/TcaUtility.php
@@ -4,20 +4,17 @@ declare(strict_types=1);
 
 namespace Xima\XmDkfzNetSite\Tca;
 
-/***
- *
- * This file is part of the "xm_dkfz_net_site" Extension for TYPO3 CMS.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- *  (c) 2022 Markus Hackel <markus.hackel@xima.de>, XIMA MEDIA GmbH
- *
- ***/
 class TcaUtility
 {
     public static array $colors = [
-        'primary', 'green', 'magenta', 'cyan', 'gray-500', 'blue-900', 'orange', 'green-light',
+        'primary',
+        'green',
+        'magenta',
+        'cyan',
+        'gray-500',
+        'blue-900',
+        'orange',
+        'green-light',
     ];
 
     public static function getItemsForColorField(bool $prependEmptyItem = true): array
@@ -32,7 +29,9 @@ class TcaUtility
 
         foreach (self::$colors as $color) {
             $items[] = [
-                $ll . $color, $color,
+                $ll . $color,
+                $color,
+                'EXT:xm_dkfz_net_site/Resources/Public/Images/icon-color-' . $color . '.svg',
             ];
         }
 
