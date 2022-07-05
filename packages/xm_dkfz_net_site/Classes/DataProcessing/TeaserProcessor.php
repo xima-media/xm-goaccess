2fa526b99bdc2aa3cf2c6c779cf5cc0b3314a463
Maik Schneider <mas@xima.de>
2022-07-05 14:16:45 +0200

    [Feature] Frontend output for teaser

---
+++ packages/xm_dkfz_net_site/Classes/DataProcessing/TeaserProcessor.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Xima\XmDkfzNetSite\DataProcessing;
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Resource\FileRepository;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\MathUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
+
+class TeaserProcessor implements DataProcessorInterface
+{
+    protected FileRepository $fileRepository;
+
+    public function __construct(FileRepository $fileRepository)
+    {
+        $this->fileRepository = $fileRepository;
+    }
+
+    public function process(
+        ContentObjectRenderer $cObj,
+        array $contentObjectConfiguration,
+        array $processorConfiguration,
+        array $processedData
+    ) {
+        $pageUid = $processedData['data']['link'] ?? '';
+        $pageUid = substr($pageUid, 14);
+
+        if (!MathUtility::canBeInterpretedAsInteger($pageUid)) {
+            return $processedData;
+        }
+
+        $qb = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('pages');
+        $qb->getRestrictions()->removeAll();
+        $page = $qb->select('*')
+            ->from('pages')
+            ->where(
+                $qb->expr()->eq('uid', $qb->createNamedParameter($pageUid, \PDO::PARAM_INT))
+            )
+            ->execute()
+            ->fetchAssociative();
+
+        if (!$pageUid) {
+            return $processedData;
+        }
+
+        // set texts from page if no override exists
+        $processedData['data']['title'] = $processedData['data']['title'] ?: $page['title'];
+        $processedData['data']['text'] = $processedData['data']['text'] ?: $page['description'];
+        $processedData['data']['color'] = $processedData['data']['color'] ?: $page['tx_xmdkfznetsite_color'];
+
+        // set the page image if no override image set
+        if (empty($processedData['files']) && $page['media']) {
+            $processedData['files'] = $this->fileRepository->findByRelation('pages', 'media', (int)$pageUid);
+        }
+
+        // set selected page as link if no links from overrides
+        if (empty($processedData['links'])) {
+            $processedData['links'] = [
+                0 => [
+                    'data' => [
+                        'link' => 't3://page?uid=' . $pageUid,
+                    ],
+                ],
+            ];
+        }
+
+        return $processedData;
+    }
+}
