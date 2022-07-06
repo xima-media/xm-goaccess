80f39e7ffea624d69e94b0425eb36b67a342cfac
Markus Hackel <mha@xima.de>
2022-07-06 14:42:56 +0200

    [Task] phpfixer & phpstan

--- packages/xm_dkfz_net_site/Configuration/TCA/Overrides/tt_content_textmedia.php
+++ packages/xm_dkfz_net_site/Configuration/TCA/Overrides/tt_content_textmedia.php
@@ -7,23 +7,23 @@ $GLOBALS['TCA']['tt_content']['types']['textmedia']['columnsOverrides']['assets'
             'allowedAspectRatios' => [
                 '3:2' => [
                     'title' => 'LLL:EXT:core/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.3_2',
-                    'value' => 3 / 2
+                    'value' => 3 / 2,
                 ],
                 '2:3' => [
                     'title' => '2:3',
-                    'value' => 2 / 3
+                    'value' => 2 / 3,
                 ],
                 '4:3' => [
                     'title' => 'LLL:EXT:core/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.4_3',
-                    'value' => 4 / 3
+                    'value' => 4 / 3,
                 ],
                 '3:4' => [
                     'title' => '3:4',
-                    'value' => 3 / 4
+                    'value' => 3 / 4,
                 ],
                 '1:1' => [
                     'title' => 'LLL:EXT:core/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.1_1',
-                    'value' => 1.0
+                    'value' => 1.0,
                 ],
             ],
             'selectedRatio' => '3:2',
