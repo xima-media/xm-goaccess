659ca4d662a89c58b9628109545c65cd9eb3e03d
Maik Schneider <mas@xima.de>
2022-07-14 11:42:19 +0200

    feat: add tca for userfeature, icon file

---
+++ packages/bw_guild/Configuration/TCA/tx_bwguild_domain_model_feature.php
@@ -0,0 +1,97 @@
+<?php
+
+return [
+    'ctrl' => [
+        'title' => 'LLL:EXT:bw_guild/Resources/Private/Language/locallang_tca.xlf:tx_bwguild_domain_model_feature',
+        'label' => 'title',
+        'crdate' => 'crdate',
+        'hideAtCopy' => true,
+        'type' => 'record_type',
+        'searchFields' => 'title',
+        'iconfile' => 'EXT:bw_guild/Resources/Public/Images/tx_bwguild_domain_model_feature.svg',
+    ],
+    'interface' => [
+        'showRecordFieldList' => 'cruser_id,pid,sys_language_uid,l10n_parent,l10n_diffsource,hidden,starttime,endtime, record_type, title, address, zip, country, description, start_date, geo_lat, geo_long, fe_user, fe_users, conditions, possibilities, contact_person, contact_mail, contact_phone, fe_group',
+    ],
+    'types' => [
+        // Skill
+        '0' => [
+            'showitem' => 'fe_user, title',
+        ],
+        // Interest
+        '1' => [
+            'showitem' => 'fe_user, title',
+        ],
+        // Hobby
+        '2' => [
+            'showitem' => 'fe_user, title',
+        ],
+    ],
+    'columns' => [
+        'record_type' => [
+            'exclude' => false,
+            'label' => 'LLL:EXT:bw_guild/Resources/Private/Language/locallang_tca.xlf:tx_bwguild_domain_model_feature.record_type',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    [
+                        'LLL:EXT:bw_guild/Resources/Private/Language/locallang_tca.xlf:tx_bwguild_domain_model_feature.record_type.0',
+                        0,
+                    ],
+                    [
+                        'LLL:EXT:bw_guild/Resources/Private/Language/locallang_tca.xlf:tx_bwguild_domain_model_feature.record_type.1',
+                        1,
+                    ],
+                    [
+                        'LLL:EXT:bw_guild/Resources/Private/Language/locallang_tca.xlf:tx_bwguild_domain_model_feature.record_type.2',
+                        2,
+                    ],
+                ],
+                'fieldWizard' => [
+                    'selectIcons' => [
+                        'disabled' => false,
+                    ],
+                ],
+                'size' => 1,
+                'maxitems' => 1,
+            ],
+        ],
+        'title' => [
+            'exclude' => false,
+            'l10n_mode' => 'prefixLangTitle',
+            'label' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_formlabel',
+            'config' => [
+                'type' => 'input',
+                'size' => 60,
+                'eval' => 'required',
+            ],
+        ],
+        'fe_user' => [
+            'exclude' => false,
+            'label' => 'LLL:EXT:bw_guild/Resources/Private/Language/locallang_tca.xlf:tx_bwguild_domain_model_offer.fe_user',
+            'config' => [
+                'type' => 'group',
+                'internal_type' => 'db',
+                'allowed' => 'fe_users',
+                'foreign_table' => 'fe_users',
+                'foreign_table_field' => 'features',
+                'size' => 1,
+                'maxitems' => 1,
+                'eval' => 'int',
+                'default' => 0,
+                'suggestOptions' => [
+                    'fe_users' => [
+                        'searchWholePhrase' => 1,
+                        'additionalSearchFields' => 'company, name, short_name, first_name, last_name',
+                    ],
+                ],
+                'fieldWizard' => [
+                    'recordsOverview' => [
+                        'disabled' => true,
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
