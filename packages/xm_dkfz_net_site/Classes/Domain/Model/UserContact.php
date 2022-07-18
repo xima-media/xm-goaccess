8d55e2f4dcdc9cf948889acbbad7960c4ac5d41e
Maik Schneider <mas@xima.de>
2022-07-18 19:00:13 +0200

    feat: add properties for UserContact

--- packages/xm_dkfz_net_site/Classes/Domain/Model/UserContact.php
+++ packages/xm_dkfz_net_site/Classes/Domain/Model/UserContact.php
@@ -2,8 +2,19 @@
 
 namespace Xima\XmDkfzNetSite\Domain\Model;
 
+use TYPO3\CMS\Extbase\Domain\Model\FrontendUserGroup;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 
 class UserContact extends AbstractEntity
 {
+    protected string $recordType = '';
+
+    protected string $position = '';
+
+    protected string $room = '';
+
+    protected bool $primaryNumber = false;
+
+    protected ?FrontendUserGroup $feGroup = null;
+
 }
