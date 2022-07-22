71a0b7bf7210b7919eb0e61e13c62953f13e7014
Maik Schneider <mas@xima.de>
2022-07-22 14:39:00 +0200

    feat: init xm_goaccess extension

---
+++ packages/xm_goaccess/Classes/Controller/BackendController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Xima\XmGoaccess\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+class BackendController extends ActionController
+{
+    protected ModuleTemplateFactory $moduleTemplateFactory;
+
+    public function __construct(
+        ModuleTemplateFactory $moduleTemplateFactory
+    ) {
+        $this->moduleTemplateFactory = $moduleTemplateFactory;
+    }
+
+    public function indexAction(): ResponseInterface
+    {
+        //$parser = GeneralUtility::makeInstance(LogParserUtility::class);
+        //$parser->run();
+        //$mails = $parser->getMessages();
+
+        //$pageRenderer = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
+        //$pageRenderer->loadRequireJsModule('TYPO3/CMS/XmMailCatcher/MailCatcher');
+
+        //$this->view->assign('mails', $mails);
+
+        $moduleTemplate = $this->moduleTemplateFactory->create($this->request);
+        $moduleTemplate->setContent($this->view->render());
+        return $this->htmlResponse($moduleTemplate->renderContent());
+    }
+}
