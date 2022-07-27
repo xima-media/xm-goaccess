0499abcf9d0752f6a5c1fae35224f25005bb6254
Maik Schneider <mas@xima.de>
2022-07-27 13:40:24 +0200

    feat: pagination for jobs

--- packages/xm_dkfz_net_prototype/source/_patterns/components/jobs/jobs.ts
+++ packages/xm_dkfz_net_prototype/source/_patterns/components/jobs/jobs.ts
@@ -3,9 +3,25 @@ import './jobs.scss'
 import app from '../basic/basic'
 
 class Jobs {
-  constructor () {
+  constructor() {
     app.log('component "job" loaded')
-    console.log('fee')
+    this.bindEvents()
+  }
+
+  bindEvents() {
+    document.querySelectorAll('.jobs__nav__button').forEach((btn) => {
+      btn.addEventListener('click', this.onMoreButtonClick.bind(this));
+    });
+  }
+
+  onMoreButtonClick(e: Event) {
+    e.preventDefault();
+    document.querySelectorAll('.jobs--list').forEach((list) => {
+      let pageNr = parseInt(list.getAttribute('data-page')) + 1
+      pageNr = parseInt(list.getAttribute('data-job-count')) <= pageNr * 8 ? -1 : pageNr
+      document.querySelector('span[data-job-count]').innerHTML = (pageNr * 8).toString()
+      list.setAttribute('data-page', pageNr.toString())
+    });
   }
 }
 
