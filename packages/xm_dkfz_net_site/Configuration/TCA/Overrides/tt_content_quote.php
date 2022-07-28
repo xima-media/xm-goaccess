b7feef46a091ac5e6085f3166f92cb2b9f5de990
Anna Bacinscaia <aba@xima.de>
2022-07-28 16:48:40 +0200

    feat: add a quote content element
    DKFZ-388: https://jira.xima.de/browse/DKFZ-338

---
+++ packages/xm_dkfz_net_site/Configuration/TCA/Overrides/tt_content_quote.php
@@ -0,0 +1,73 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'Quote',
+        'quote',
+        'content-quote',
+    ],
+    'image',
+    'after'
+);
+$GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['quote'] = 'content-quote';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', [
+    'tx_xmdkfznetsite_author' => [
+        'exclude' => 0,
+        'label' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:quote.author',
+        'description' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:quote.author.description',
+        'config' => [
+            'type' => 'input',
+            'eval' => 'trim,required',
+        ],
+    ],
+    'tx_xmdkfznetsite_function' => [
+        'exclude' => 0,
+        'label' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:quote.author.function',
+        'description' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:quote.author.function.description',
+        'config' => [
+            'type' => 'input',
+            'eval' => 'trim,required',
+        ],
+    ],
+]);
+
+$GLOBALS['TCA']['tt_content']['palettes']['quote'] = [
+    'label' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:quote.palette',
+    'showitem' => 'tx_xmdkfznetsite_author,--linebreak--,tx_xmdkfznetsite_function',
+];
+
+$GLOBALS['TCA']['tt_content']['types']['quote'] = [
+    'showitem' => '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                    --palette--;;general,
+                    --palette--;;header,
+                    bodytext,
+                    tx_xmdkfznetsite_author,
+                    tx_xmdkfznetsite_function,
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                    --palette--;;infoboxAppearance,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                    --palette--;;language,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                    --palette--;;hidden,
+                    --palette--;;access,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+                    categories,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+                    rowDescription,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,',
+    'columnsOverrides' => [
+        'sectionIndex' => [
+            'config' => [
+                'default' => 0,
+            ],
+        ],
+        'bodytext' => [
+            'config' => [
+                'enableRichtext' => false,
+            ],
+        ],
+    ],
+];
