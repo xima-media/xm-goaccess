a4081de3c317348b7eaf34c0f68f28e5c86b7248
Anna Bacinscaia <aba@xima.de>
2022-07-29 12:18:48 +0200

    fix: improve quote template
    improve tca of quote content element
    DKFZ-388: https://jira.xima.de/browse/DKFZ-338

--- packages/xm_dkfz_net_site/Configuration/TCA/Overrides/tt_content_quote.php
+++ packages/xm_dkfz_net_site/Configuration/TCA/Overrides/tt_content_quote.php
@@ -4,7 +4,7 @@
     'tt_content',
     'CType',
     [
-        'Quote',
+        'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:quote',
         'quote',
         'content-quote',
     ],
@@ -20,7 +20,7 @@ $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['quote'] = 'content-qu
         'description' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:quote.author.description',
         'config' => [
             'type' => 'input',
-            'eval' => 'trim,required',
+            'eval' => 'trim',
         ],
     ],
     'tx_xmdkfznetsite_function' => [
@@ -29,7 +29,7 @@ $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['quote'] = 'content-qu
         'description' => 'LLL:EXT:xm_dkfz_net_site/Resources/Private/Language/locallang.xlf:quote.author.function.description',
         'config' => [
             'type' => 'input',
-            'eval' => 'trim,required',
+            'eval' => 'trim',
         ],
     ],
 ]);
@@ -44,8 +44,7 @@ $GLOBALS['TCA']['tt_content']['types']['quote'] = [
                     --palette--;;general,
                     --palette--;;header,
                     bodytext,
-                    tx_xmdkfznetsite_author,
-                    tx_xmdkfznetsite_function,
+                    --palette--;;quote,
                 --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
                     --palette--;;infoboxAppearance,
                 --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
