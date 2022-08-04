3f4aa07e3536e5e9cdf5b4c9441526b82d0f1386
Eliseo Malo <ema@xima.de>
2022-08-04 16:40:17 +0200

    feat: adjust teaser component (DKFZ-280)

--- packages/xm_dkfz_net_prototype/source/app.ts
+++ packages/xm_dkfz_net_prototype/source/app.ts
@@ -211,6 +211,9 @@ import './_patterns/components/login/login'
 /** @section 2.49 List */
 import './_patterns/components/list/list'
 
+/** @section 2.50 Teaser */
+import './_patterns/components/teaser/teaser'
+
 /** @section 2.99 Debug */
 import './_patterns/components/debug/debug-menu'
 import './_patterns/components/debug/debug-gridlines'
