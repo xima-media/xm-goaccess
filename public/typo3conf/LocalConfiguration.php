8d97fdc9c819e4f901b0a315f8993bfcc704e147
Maik Schneider <mas@xima.de>
2022-08-04 08:35:23 +0200

    chore: remove test-line

--- public/typo3conf/LocalConfiguration.php
+++ public/typo3conf/LocalConfiguration.php
@@ -82,7 +82,6 @@ return [
                         'backend',
                         'frontend',
                     ],
-                    'test' => 'fef',
                 ],
             ],
         ],
