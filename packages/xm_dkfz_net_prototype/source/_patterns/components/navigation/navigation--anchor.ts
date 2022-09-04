16e9d256377cdfdba6bd5e05de7485233ca6fba9
Yuliia Sorokina <yuliia.sorokina@xima.de>
2022-09-04 19:46:22 +0200

    feat: menu anchor navigation viewport laptop (DKFZ-277)

--- packages/xm_dkfz_net_prototype/source/_patterns/components/navigation/navigation--anchor.ts
+++ packages/xm_dkfz_net_prototype/source/_patterns/components/navigation/navigation--anchor.ts
@@ -25,209 +25,137 @@ import app from '../basic/basic'
  */
 
 class NavigationAnchor {
-    timelineItemWidth: number;
-    private _navigationLinks: NodeListOf<HTMLElement>;
-        get scrollWidth(): number {
-            return this._scrollWidth;
-        }
+    constructor() {
+        app.log('component "anchor navigation" loaded')
 
-        set scrollWidth(value: number) {
-            this._scrollWidth = value;
+        // only if element on the page
+        if (document.querySelectorAll<HTMLElement>('.navigation--anchor').length) {
+            // methods
+            this.events()
         }
-        private _scrollWidth: number;
-        get containerItemEl(): NodeListOf<HTMLElement> {
-            return this._containerItemEl;
-        }
+    }
 
-        set containerItemEl(value: NodeListOf<HTMLElement>) {
-            this._containerItemEl = value;
-        }
-        private _containerItemEl: NodeListOf<HTMLElement>;
-        get containerItemsEl(): HTMLElement {
-            return this._containerItemsEl;
-        }
+    /**
+     * Events
+     */
+    events () {
+        // variables
+        const nav = document.querySelector<HTMLElement>('.navigation--anchor');
+        const navItems = nav.querySelector<HTMLElement>('.navigation__items');
+        const navItem = navItems.querySelectorAll<HTMLElement>('.navigation__link');
+        const sections = document.querySelectorAll<HTMLElement>('.content-wrapper')
+        const hScroll = nav.querySelector<HTMLElement>('.horizontal-scroll');
+        const btnScrollLeft = hScroll.querySelector<HTMLButtonElement>('.navigation__button.left');
+        const btnScrollRight = hScroll.querySelector<HTMLButtonElement>('.navigation__button.right');
 
-        set containerItemsEl(value: HTMLElement) {
-            this._containerItemsEl = value;
-        }
-        private _containerItemsEl: HTMLElement;
-        get buttonScrollNextEl(): HTMLButtonElement {
-            return this._buttonScrollNextEl;
-        }
 
-        set buttonScrollNextEl(value: HTMLButtonElement) {
-            this._buttonScrollNextEl = value;
-        }
-        private _buttonScrollNextEl: HTMLButtonElement;
-        get buttonScrollPrevEl(): HTMLButtonElement {
-            return this._buttonScrollPrevEl;
-        }
-
-        set buttonScrollPrevEl(value: HTMLButtonElement) {
-            this._buttonScrollPrevEl = value;
-        }
-        private _buttonScrollPrevEl: HTMLButtonElement;
-        get buttonEl(): NodeListOf<HTMLButtonElement> {
-            return this._buttonEl;
-        }
-
-        set buttonEl(value: NodeListOf<HTMLButtonElement>) {
-            this._buttonEl = value;
-        }
-        private _buttonEl: NodeListOf<HTMLButtonElement>;
-        get container(): HTMLElement {
-            return this._container;
-        }
-
-        set container(value: HTMLElement) {
-            this._container = value;
-        }
-        private _container: HTMLElement;
-        constructor() {
-            app.log('component "anchor navigation" loaded')
-
-            // only if element on the page
-            if (document.querySelectorAll<HTMLElement>('.navigation--anchor').length) {
-
-                this._container = document.querySelector<HTMLElement>('.navigation--anchor')
-                this._buttonEl = this._container.querySelectorAll<HTMLButtonElement>('.navigation__button')
-                this._buttonScrollPrevEl = this._container.querySelector<HTMLButtonElement>('.navigation__button.left')
-                this._buttonScrollNextEl = this._container.querySelector<HTMLButtonElement>('.navigation__button.right')
-                this._containerItemsEl = this._container.querySelector<HTMLElement>('.navigation__items')
-                this._containerItemEl = this._containerItemsEl.querySelectorAll<HTMLElement>('.navigation__item')
-                this._scrollWidth = 200
-
-                // methods
-                this.events()
-            }
-        }
+        let maxScroll = -hScroll.scrollWidth + navItems.offsetWidth;
+        let currentScrollPosition = 0;
+        let scrollAmount = hScroll.offsetWidth / 4;
 
         /**
-         * Events
+         * menu desktop
          */
-        events () {
-            // variables
-            const self = this
+        // active links
+        navItem.forEach(el => {
+            el.addEventListener('click', function(){
+                navItem.forEach(nav=> {
+                  nav.classList.remove('active')
+                  nav.parentElement.classList.remove('active')
+                  }
+                );
 
-            // active links
-            let nav = document.querySelector('.navigation--anchor');
-            let navItems = nav.querySelector('.navigation__items');
-            let navItem = navItems.querySelectorAll('.navigation__link');
-            let sections = document.querySelectorAll('.content-wrapper')
-
-            navItem.forEach(el => {
-                el.addEventListener('click', function(){
-                    navItem.forEach(nav=> {
-                      nav.classList.remove('active')
-                      nav.parentElement.classList.remove('active')
-                      }
-                    );
-
-                    this.classList.add('active')
-                    if(this.classList.contains('active')) {
-                      this.parentElement.classList.add('active')
-                    }
-                })
+                this.classList.add('active')
+                if(this.classList.contains('active')) {
+                  this.parentElement.classList.add('active')
+                }
             })
+        })
 
-            // change the active class on anchor menu item
-            const observer = new IntersectionObserver(entries => {
-                entries.forEach(entry => {
-                    if(entry.isIntersecting) {
-                      let entryTarget = entry.target;
-                      let entryTargetId = '#' + entryTarget.getAttribute('id')
+        // change the active class on anchor menu item
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if(entry.isIntersecting) {
+                  let entryTarget = entry.target;
+                  let entryTargetId = '#' + entryTarget.getAttribute('id')
 
-                        navItem.forEach(li => {
-                            let liHref = li.getAttribute('href')
+                    navItem.forEach(li => {
+                        let liHref = li.getAttribute('href')
 
-                            li.classList.remove('active')
-                            li.parentElement.classList.remove('active')
+                        li.classList.remove('active')
+                        li.parentElement.classList.remove('active')
 
-                            if(liHref === entryTargetId) {
-                                li.classList.add('active')
-                                li.parentElement.classList.add('active')
-                            }
-                        })
-                    }
-                })
-            }, {
-                threshold: 0.75
+                        if(liHref === entryTargetId) {
+                            li.classList.add('active')
+                            li.parentElement.classList.add('active')
+                        }
+                    })
+                }
             })
+        }, {
+            threshold: 0.75
+        })
 
-            // filter the section for observe
-            Array.from(sections).filter(section => {
-                const sectionId = '#' + section.getAttribute('id')
+        // filter the section for observe
+        Array.from(sections).filter(section => {
+            const sectionId = '#' + section.getAttribute('id')
 
-                navItem.forEach(li => {
-                    let liHref = li.getAttribute('href')
+            navItem.forEach(li => {
+                let liHref = li.getAttribute('href')
 
-                    if(liHref === sectionId) {
-                        observer.observe(section)
-                    }
-                })
+                if(liHref === sectionId) {
+                    observer.observe(section)
+                }
             })
+        })
 
-            // buttons
-            self._buttonEl.forEach((button) => button.addEventListener('click', () => self.scrollToTimelineItem({buttonEl: button})))
-
-            // horizontal scrolling
-            self._containerItemEl.forEach((item) => self.activateTimelineItemByScrolling({item: item}))
-        }
 
         /**
-         * Scrolling
+         * menu laptop
          */
-        scrollToTimelineItem ({buttonEl}: { buttonEl: any }) {
-            // variables
-            const self = this
-            const scrollPositionX = self._containerItemsEl.scrollLeft
+        // Button show/hide
+        btnScrollLeft.style.opacity = '0';
 
-            // scroll direction
-            if (buttonEl.classList.contains('right')) {
-                self.timelineItemWidth = scrollPositionX + self._scrollWidth
-            } else if (buttonEl.classList.contains('left')) {
-                self.timelineItemWidth = scrollPositionX - self._scrollWidth
+        if(maxScroll <= 0) {
+            btnScrollRight.style.opacity = '1';
+        } else {
+            btnScrollRight.style.opacity = '0';
+        }
+
+        function scrollHorizontally(val: number) {
+            currentScrollPosition += (val * scrollAmount);
+
+            if (currentScrollPosition >= 0) {
+                currentScrollPosition = 0;
+                btnScrollLeft.style.opacity = '0'
+            } else {
+                btnScrollLeft.style.opacity = '1'
             }
 
-            // smooth scrolling
-            self._containerItemsEl.scrollTo({
-                left: self.timelineItemWidth,
-                behavior: 'smooth'
-            })
+            if (currentScrollPosition <= maxScroll) {
+                currentScrollPosition = maxScroll;
+                btnScrollRight.style.opacity = '0';
+            } else {
+                btnScrollRight.style.opacity = '1';
+            }
+
+            navItems.style.left = currentScrollPosition + 'px';
         }
 
-        /**
-         * Button show/hide
-         */
-        activateTimelineItemByScrolling ({item}: { item: any }) {
-            // variables
-            const self = this
+        btnScrollLeft.addEventListener('click', () => scrollHorizontally(1))
+        btnScrollRight.addEventListener('click', () => scrollHorizontally(-1))
 
-            // observe
-            new IntersectionObserver((entries, observer) => {
-                entries.forEach(entry => {
-                    if (entry.isIntersecting) {
-                        // prev button
-                        if (entry.target.previousElementSibling) {
-                            self._buttonScrollPrevEl.removeAttribute('disabled')
-                        } else {
-                            self._buttonScrollPrevEl.setAttribute('disabled', 'disabled')
-                        }
-
-                        // next button
-                        if (entry.target.nextElementSibling) {
-                            self._buttonScrollNextEl.removeAttribute('disabled')
-                        } else {
-                            self._buttonScrollNextEl.setAttribute('disabled', 'disabled')
-                        }
-                    }
-                })
-            }, {
-                root: document,
-                rootMargin: '0px 0px 0px 0px',
-                threshold: 1
-            }).observe(item)
-        }
+        // sticky menu
+        // window.addEventListener('scroll', () => {
+        //     const sticky = nav.offsetTop;
+        //
+        //     if (window.pageYOffset >= sticky) {
+        //         nav.classList.add("sticky")
+        //     } else {
+        //         nav.classList.remove("sticky");
+        //     }
+        // })
+    }
 }
 
 /**
