bd7c10df508122a2de1bd45210abb592aacd4ab5
Maik Schneider <mas@xima.de>
2022-09-12 13:42:09 +0200

    feat: add dashboard preset

---
+++ packages/xm_goaccess/Configuration/Backend/DashboardPresets.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'goaccess' => [
+        'title' => 'LLL:EXT:xm_goaccess/Resources/Private/Language/locallang.xlf:dashboard.goaccess',
+        'description' => 'LLL:EXT:xm_goaccess/Resources/Private/Language/locallang.xlf:dashboard.goaccess.description',
+        'iconIdentifier' => 'module-goaccess',
+        'defaultWidgets' => ['goaccessUniqueVisitors', 'goaccessVisitTime', 'goaccessBrowsers', 'goaccessOs', 'goaccessRequests'],
+        'showInWizard' => true,
+    ],
+];
