3df5e1479c2dcfdde9d54a34759ad44c61887148
Maik Schneider <mas@xima.de>
2022-09-14 09:27:13 +0200

    feat: remove infoboxAppearance palette, use frames

--- packages/xm_dkfz_net_site/Configuration/TCA/Overrides/tt_content_quote.php
+++ packages/xm_dkfz_net_site/Configuration/TCA/Overrides/tt_content_quote.php
@@ -46,7 +46,7 @@ $GLOBALS['TCA']['tt_content']['types']['quote'] = [
                     bodytext,
                     --palette--;;quote,
                 --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
-                    --palette--;;infoboxAppearance,
+                    --palette--;;frames,
                     --palette--;;appearanceLinks,
                 --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
                     --palette--;;language,
