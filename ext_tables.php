c638da7794f750904ef12ab25f87c0a5573faf86
Maik Schneider <mas@xima.de>
2022-09-30 15:00:16 +0200

    feat: disable backend module if no html path set

--- ext_tables.php
+++ ext_tables.php
@@ -1,16 +1,18 @@
 <?php
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-    'XmGoaccess',
-    'system',
-    'goaccess',
-    '',
-    [
-        \Xima\XmGoaccess\Controller\BackendController::class => 'index',
-    ],
-    [
-        'access' => 'admin',
-        'iconIdentifier' => 'module-goaccess',
-        'labels' => 'LLL:EXT:xm_goaccess/Resources/Private/Language/locallang_mod.xlf',
-    ]
-);
+if (isset($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['xm_goaccess']['html_path']) && $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['xm_goaccess']['html_path']) {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        'XmGoaccess',
+        'system',
+        'goaccess',
+        '',
+        [
+            \Xima\XmGoaccess\Controller\BackendController::class => 'index',
+        ],
+        [
+            'access' => 'admin',
+            'iconIdentifier' => 'module-goaccess',
+            'labels' => 'LLL:EXT:xm_goaccess/Resources/Private/Language/locallang_mod.xlf',
+        ]
+    );
+}
