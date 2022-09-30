ca610d4d3513d5d7838325712a117a786793de86
Eliseo Malo <ema@xima.de>
2022-09-30 13:47:47 +0200

    feat: limit available sys_file_metadata fields (DKFZ-392)

--- packages/xm_dkfz_net_site/ext_localconf.php
+++ packages/xm_dkfz_net_site/ext_localconf.php
@@ -2,6 +2,7 @@
 
 // Register Page- and UserTS config
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('@import "EXT:xm_dkfz_net_site/Configuration/TSconfig/User.tsconfig"');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('@import "EXT:xm_dkfz_net_site/Configuration/TSconfig/Page.tsconfig"');
 
 // Register DataHandler Hook
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['xm_dkfz_net_site'] =
