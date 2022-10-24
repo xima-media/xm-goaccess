669bf8790fb236f9260e9175c4150212170b1f99
Maik Schneider <mas@xima.de>
2022-10-24 15:24:27 +0200

    chore: phpfixer

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '11.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-12.99.99'
+            'typo3' => '11.0.0-12.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
