a15ae10dc5a65534ca24dd7bec4a7a11e1985f49
Maik Schneider <mas@xima.de>
2022-10-24 17:53:01 +0200

    feat: remove goaccess from packages, require via composer

--- packages/xm_goaccess/Configuration/Backend/DashboardWidgetGroups.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'goaccess' => [
-        'title' => 'LLL:EXT:xm_goaccess/Resources/Private/Language/locallang.xlf:widgetGroup.goaccess.title',
-    ],
-];
--- packages/xm_goaccess/Configuration/Icons.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'module-goaccess' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:xm_goaccess/Resources/Public/Images/module-goaccess.svg',
-    ],
-];
