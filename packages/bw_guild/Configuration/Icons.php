d722e491b4d23f1bbb00c9a3882bc33c6d220474
Maik Schneider <mas@xima.de>
2022-10-25 15:48:24 +0200

    chore: phpfixer

--- packages/bw_guild/Configuration/Icons.php
+++ packages/bw_guild/Configuration/Icons.php
@@ -3,46 +3,46 @@
 return [
     'tx_bwguild_userlist' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:bw_guild/Resources/Public/Images/tt_content_userlist.svg'
+        'source' => 'EXT:bw_guild/Resources/Public/Images/tt_content_userlist.svg',
     ],
     'tx_bwguild_usershow' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:bw_guild/Resources/Public/Images/tt_content_usershow.svg'
+        'source' => 'EXT:bw_guild/Resources/Public/Images/tt_content_usershow.svg',
     ],
     'tx_bwguild_search' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:bw_guild/Resources/Public/Images/tt_content_search.svg'
+        'source' => 'EXT:bw_guild/Resources/Public/Images/tt_content_search.svg',
     ],
     'tx_bwguild_offerlist' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:bw_guild/Resources/Public/Images/tt_content_offerlist.svg'
+        'source' => 'EXT:bw_guild/Resources/Public/Images/tt_content_offerlist.svg',
     ],
     'tx_bwguild_offershow' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:bw_guild/Resources/Public/Images/tt_content_offershow.svg'
+        'source' => 'EXT:bw_guild/Resources/Public/Images/tt_content_offershow.svg',
     ],
     'tx_bwguild_domain_model_offer' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:bw_guild/Resources/Public/Images/tx_bwguild_domain_model_offer.svg'
+        'source' => 'EXT:bw_guild/Resources/Public/Images/tx_bwguild_domain_model_offer.svg',
     ],
     'tx_bwguild_domain_model_offer-0' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:bw_guild/Resources/Public/Images/tx_bwguild_domain_model_offer.0.svg'
+        'source' => 'EXT:bw_guild/Resources/Public/Images/tx_bwguild_domain_model_offer.0.svg',
     ],
     'tx_bwguild_domain_model_offer-1' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:bw_guild/Resources/Public/Images/tx_bwguild_domain_model_offer.1.svg'
+        'source' => 'EXT:bw_guild/Resources/Public/Images/tx_bwguild_domain_model_offer.1.svg',
     ],
     'tx_bwguild_domain_model_offer-2' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:bw_guild/Resources/Public/Images/tx_bwguild_domain_model_offer.2.svg'
+        'source' => 'EXT:bw_guild/Resources/Public/Images/tx_bwguild_domain_model_offer.2.svg',
     ],
     'tx_bwguild_domain_model_offer-3' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:bw_guild/Resources/Public/Images/tx_bwguild_domain_model_offer.3.svg'
+        'source' => 'EXT:bw_guild/Resources/Public/Images/tx_bwguild_domain_model_offer.3.svg',
     ],
     'tx_bwguild_domain_model_feature' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        'source' => 'EXT:bw_guild/Resources/Public/Images/tx_bwguild_domain_model_feature.svg'
-    ]
+        'source' => 'EXT:bw_guild/Resources/Public/Images/tx_bwguild_domain_model_feature.svg',
+    ],
 ];
