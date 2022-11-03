d1d325b916111798d62f120d25f95f9983050c95
Markus Hackel <mha@xima.de>
2022-11-03 10:41:50 +0100

    fix: add sorting for user to search results (DKFZ-470)

--- packages/xm_dkfz_net_prototype/source/_patterns/components/hero-form/hero-form.ts
+++ packages/xm_dkfz_net_prototype/source/_patterns/components/hero-form/hero-form.ts
@@ -56,6 +56,7 @@ class HeroForm {
   protected bindSearchFormEvents() {
     this.bindResultListCheckbox()
     this.bindResultListResetButton()
+    this.bindResultListSortingSelect()
   }
 
   protected bindResultListCheckbox() {
@@ -91,6 +92,23 @@ class HeroForm {
     });
 
     form.submit()
+  }
+
+  protected bindResultListSortingSelect() {
+    document.querySelectorAll('select[name="field__input--name-sorting"]').forEach((sortingSelect: HTMLSelectElement) => {
+      sortingSelect.addEventListener('change', this.onResultListSortingSelectChange.bind(this))
+    });
+  }
+
+  protected onResultListSortingSelectChange(e: Event) {
+    const selectField = e.currentTarget as HTMLSelectElement
+    const selectedOption = selectField.selectedOptions[0] as HTMLOptionElement
+
+    console.log(selectedOption)
+
+    if (selectedOption) {
+      location.href = selectedOption.value
+    }
   }
 
 }
