d0c1ed938dbd59459d034743adc18f648bbf8459
Maik Schneider <mas@xima.de>
2022-11-17 13:24:17 +0100

    feat: add headline with table count to dkfz backend module

--- packages/xm_dkfz_net_site/Resources/Public/JavaScript/BackendModuleDkfz.js
+++ packages/xm_dkfz_net_site/Resources/Public/JavaScript/BackendModuleDkfz.js
@@ -10,12 +10,14 @@ define([], function () {
         this.cacheDom()
         this.bindTypeSwitchButtons()
         this.bindSearchInput()
+        this.updateEntryCount()
     }
 
     BackendModuleDkfz.cacheDom = function () {
         this.table = document.querySelector('#dkfz-table')
         this.typeButtons = document.querySelectorAll('.type-switch')
         this.searchInput = document.querySelector('#dkfz-search input.form-control')
+        this.entryCount = document.querySelectorAll('span[data-entry-count]')
     }
 
     BackendModuleDkfz.bindTypeSwitchButtons = function () {
@@ -41,6 +43,13 @@ define([], function () {
         this.table.querySelectorAll('tbody tr[data-type="' + newType + '"]').forEach(tr => {
             tr.classList.remove('hidden')
         })
+
+        this.updateEntryCount()
+    }
+
+    BackendModuleDkfz.updateEntryCount = function() {
+        const count = this.table.querySelectorAll('tbody tr:not([class^="hidden"])').length
+        this.entryCount.forEach(span => span.innerHTML = count.toString());
     }
 
     BackendModuleDkfz.bindSearchInput = function () {
@@ -58,6 +67,8 @@ define([], function () {
                 tr.classList.remove('hidden-filter')
             }
         })
+
+        this.updateEntryCount()
     }
 
     // To let the module be a dependency of another module, we return our object
