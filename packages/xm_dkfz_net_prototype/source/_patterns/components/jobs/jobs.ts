2cab295253d4a2b5b52e6eb9721f64f17b96e3bf
Maik Schneider <mas@xima.de>
2022-11-22 17:00:08 +0100

    feat: dynamic pager for jobs

--- packages/xm_dkfz_net_prototype/source/_patterns/components/jobs/jobs.ts
+++ packages/xm_dkfz_net_prototype/source/_patterns/components/jobs/jobs.ts
@@ -3,6 +3,43 @@ import './jobs.scss'
 class Jobs {
   constructor() {
     this.bindEvents()
+    this.initPagination()
+  }
+
+  initPagination() {
+    document.querySelectorAll('.jobs--list--pager-not-loaded').forEach(div => div.classList.remove('jobs--list--pager-not-loaded'))
+    this.updatePager()
+  }
+
+  updatePager() {
+    document.querySelectorAll('.jobs--list').forEach((list) => {
+      list.querySelectorAll('a').forEach(a => a.classList.add('hidden-pager'))
+      const notHiddenElements = list.querySelectorAll('a:not(.hidden-filter-category):not(.hidden-filter-place)');
+      notHiddenElements.forEach(a => a.classList.remove('hidden-pager'))
+
+      const count = notHiddenElements.length
+      let pageNr = parseInt(list.getAttribute('data-page'))
+      const visibleCount = 8 * pageNr;
+      notHiddenElements.forEach((element, i) => {
+        if (i > visibleCount) {
+          element.classList.add('hidden-pager')
+        }
+      })
+      
+      // update counter
+      document.querySelector('span[data-job-count]').innerHTML = (visibleCount).toString()
+      document.querySelector('span[data-all-job-count]').innerHTML = count.toString()
+
+      // hide pager
+      if (visibleCount >= count) {
+        list.setAttribute('data-page', '-1')
+      }
+
+      // hide + show not found message
+      if (notHiddenElements.length === 0) {
+        list.setAttribute('data-page', '-2')
+      }
+    });
   }
 
   bindEvents() {
@@ -21,6 +58,8 @@ class Jobs {
     const filterFor = selectElement.getAttribute('id');
     const filterValue = selectElement.value;
 
+    document.querySelectorAll('.jobs--list').forEach(list => list.setAttribute('data-page', '1'));
+
     document.querySelectorAll('.jobs--list a').forEach(link => {
       link.classList.remove('hidden-filter-' + filterFor);
       const linkValue = link.getAttribute('data-' + filterFor);
@@ -29,16 +68,18 @@ class Jobs {
       }
     })
 
+    this.updatePager()
   }
 
   onMoreButtonClick(e: Event) {
     e.preventDefault();
     document.querySelectorAll('.jobs--list').forEach((list) => {
       let pageNr = parseInt(list.getAttribute('data-page')) + 1
-      pageNr = parseInt(list.getAttribute('data-job-count')) <= pageNr * 8 ? -1 : pageNr
       document.querySelector('span[data-job-count]').innerHTML = (pageNr * 8).toString()
       list.setAttribute('data-page', pageNr.toString())
     });
+
+    this.updatePager()
   }
 }
 
