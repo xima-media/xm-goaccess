b781c2fd7f9c4ebfaba892ff9a7bd306e97f1868
Maik Schneider <mas@xima.de>
2022-11-25 10:37:02 +0100

    feat: add config for image_autoresize

---
+++ public/typo3conf/image_autoresize.config.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'directories' => '1:/',
+    'file_types' => 'jpg,jpeg,png',
+    'threshold' => '500K',
+    'max_width' => '2048',
+    'max_height' => '2048',
+    'max_size' => '200M',
+    'auto_orient' => '1',
+    'conversion_mapping' => 'ai => jpg,bmp => jpg,pcx => jpg,tga => jpg,tif => jpg,tiff => jpg',
+    'keep_metadata' => '0',
+    'resize_png_with_alpha' => '0',
+];
