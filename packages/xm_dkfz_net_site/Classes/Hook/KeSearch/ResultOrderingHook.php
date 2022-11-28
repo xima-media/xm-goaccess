3fbdad3772cac5f5e4807e78a23e874f75c9b186
Markus Hackel <mha@xima.de>
2022-11-28 16:39:52 +0100

    chore: phpfixer

--- packages/xm_dkfz_net_site/Classes/Hook/KeSearch/ResultOrderingHook.php
+++ packages/xm_dkfz_net_site/Classes/Hook/KeSearch/ResultOrderingHook.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Xima\XmDkfzNetSite\Hook\KeSearch;
 
-
 use Tpwd\KeSearch\Lib\Db;
 
 class ResultOrderingHook
 {
-    public function getOrdering(string &$orderBy, Db $parentObject): void {
+    public function getOrdering(string &$orderBy, Db $parentObject): void
+    {
         if ($orderBy !== '') {
             $orderBy = 'customranking DESC, ' . $orderBy;
         } else {
