8909f8b086f1b24f1d0220ce8032b1e794d7fb0c
Maik Schneider <mas@xima.de>
2022-12-06 18:02:01 +0100

    feat: bookmark sidebar style, add news preview image to output

--- packages/xm_dkfz_net_site/Classes/EventListener/UserInfoApiEvent.php
+++ packages/xm_dkfz_net_site/Classes/EventListener/UserInfoApiEvent.php
@@ -4,9 +4,17 @@ namespace Xima\XmDkfzNetSite\EventListener;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\RootlineUtility;
+use Xima\XmDkfzNetSite\Domain\Repository\NewsRepository;
 
 class UserInfoApiEvent
 {
+    protected NewsRepository $newsRepository;
+
+    public function __construct(NewsRepository $newsRepository)
+    {
+        $this->newsRepository = $newsRepository;
+    }
+
     public function __invoke(\Blueways\BwGuild\Event\UserInfoApiEvent $event): void
     {
         $userinfo = $event->getUserinfo();
@@ -18,7 +26,16 @@ class UserInfoApiEvent
             $bookmarks['pages'][$key]['rootline'] = array_reverse($rootline, true);
         }
 
+        if (isset($bookmarks['tx_news_domain_model_news']) && $bookmarks['tx_news_domain_model_news']) {
+            $newsUids = array_keys($bookmarks['tx_news_domain_model_news']);
+            $sysFileReferences = $this->newsRepository->getPreviewSysFileReferences($newsUids);
+            foreach ($sysFileReferences as $newsUid => $referenceUid) {
+                $bookmarks['tx_news_domain_model_news'][$newsUid]['image'] = $referenceUid;
+            }
+        }
+
         $userinfo->bookmarks = $bookmarks;
         $event->setUserinfo($userinfo);
     }
+
 }
