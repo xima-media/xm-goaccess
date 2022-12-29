6808dca5db7289e8db7bc8f06ccc940c82fc0a71
Maik Schneider <mas@xima.de>
2022-12-29 11:34:27 +0100

    chore: refactor video-player.ts

--- packages/xm_dkfz_net_site/Resources/Private/TypeScript/components/video-player.ts
+++ packages/xm_dkfz_net_site/Resources/Private/TypeScript/components/video-player.ts
@@ -6,58 +6,50 @@ class VideoPlayer {
     this.playButtonEl = document.querySelectorAll('.--play-video')
 
     // methods
-    this.events()
+    this.bindEvents()
   }
 
-  events () {
-    const self = this
-
+  bindEvents (): void {
     // click: play button
-    self.playButtonEl.forEach((button) => button.addEventListener('click', () => self.playExternalVideo(button)))
+    this.playButtonEl.forEach((button) => button.addEventListener('click', () => this.playExternalVideo(button)))
 
     // media slider: remove
     document.addEventListener('video:allowed', () => {
-      self.playAllExternalVideos()
+      this.playAllExternalVideos()
     })
   }
 
-  playAllExternalVideos () {
-    const self = this
-
-    self.playButtonEl.forEach((button) => self.playExternalVideo(button))
+  playAllExternalVideos (): void {
+    this.playButtonEl.forEach((button) => this.playExternalVideo(button))
   }
 
-  playExternalVideo (button: HTMLButtonElement) {
+  playExternalVideo (button: HTMLButtonElement): void {
     const videoPlayerEl: HTMLDivElement = button.closest('.video-player')
-    const videoId: String = videoPlayerEl.dataset.videoId
-    const videoMimeType: String = videoPlayerEl.dataset.videoMimeType
+    const videoId = videoPlayerEl.dataset.videoId
+    const videoMimeType = videoPlayerEl.dataset.videoMimeType
     const fullScreen = videoPlayerEl.dataset.fullScreen === 'true' ? 'allowfullscreen="true"' : ''
-    const title: String = videoPlayerEl.dataset.title
-    const videoSrc: String = this.buildVideoUrlByMimetype(videoMimeType, videoId)
+    const title = videoPlayerEl.dataset.title
+    const videoSrc = this.buildVideoUrlByMimetype(videoMimeType, videoId)
 
     // append video
-    if (videoSrc) {
-      videoPlayerEl.insertAdjacentHTML('beforeend', '<iframe class="video-player__iframe" src="' + videoSrc + '" title="' + title + '" ' + fullScreen + '></iframe>')
+    if (videoSrc !== '') {
+      const iframeElement = `<iframe class="video-player__iframe" src="${videoSrc}" title="${title}" ${fullScreen}></iframe>`
+      videoPlayerEl.insertAdjacentHTML('beforeend', iframeElement)
     } else if (videoMimeType === 'video/mp4') {
       videoPlayerEl.classList.add('video-player--hide-elements')
     }
   }
 
-  buildVideoUrlByMimetype (mimeType: String, videoId: String): String {
-    let videoEmbedUrl
-
-    switch (mimeType) {
-      case 'video/youtube':
-        videoEmbedUrl = `https://www.youtube.com/embed/${videoId}`
-        break
-      case 'video/vimeo':
-        videoEmbedUrl = `https://player.vimeo.com/video/${videoId}`
-        break
-      default:
-        break
+  buildVideoUrlByMimetype (mimeType: string, videoId: string): string {
+    if (mimeType === 'video/youtube') {
+      return `https://www.youtube.com/embed/${videoId}`
     }
 
-    return videoEmbedUrl
+    if (mimeType === 'video/vimeo') {
+      return `https://player.vimeo.com/video/${videoId}`
+    }
+
+    return ''
   }
 }
 
