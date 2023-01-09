ae011de3670d4f7fd4603b92bd672fc5ffb65e4f
Eliseo Malo <ema@xima.de>
2023-01-09 09:06:42 +0100

    chore: use local db for importing post start

--- .ddev/import-database.sh
+++ .ddev/import-database.sh
@@ -5,7 +5,7 @@ DB_LOADED=$(echo "select * from pages" | vendor/bin/typo3cms database:import)
 if test -z "$DB_LOADED"
 then
   echo 'loading db'
-  ./vendor/bin/dep db:decompress --options=dumpcode:dkfzStarterDb
-  ./vendor/bin/dep db:import --options=dumpcode:dkfzStarterDb
-  ./vendor/bin/dep db:compress --options=dumpcode:dkfzStarterDb
+  ./vendor/bin/dep db:decompress local --options=dumpcode:dkfzStarterDb
+  ./vendor/bin/dep db:import local --options=dumpcode:dkfzStarterDb
+  ./vendor/bin/dep db:compress local --options=dumpcode:dkfzStarterDb
 fi
