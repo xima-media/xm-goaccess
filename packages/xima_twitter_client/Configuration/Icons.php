92a2efb9209e138763fd5495defd31b444bea532
Maik Schneider <mas@xima.de>
2023-01-10 07:31:40 +0100

    feat: add icons

---
+++ packages/xima_twitter_client/Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'folder-contains-twitter' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:xima_twitter_client/Resources/Public/Icons/icon-folder-twitter.svg',
+    ],
+];
