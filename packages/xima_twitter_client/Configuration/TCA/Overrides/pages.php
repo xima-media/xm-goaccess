de0792699db45290a7c88c3523493feae7ca7f94
Maik Schneider <mas@xima.de>
2023-01-10 07:50:25 +0100

    feat: new page module type

---
+++ packages/xima_twitter_client/Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,8 @@
+<?php
+
+$GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
+    0 => 'Twitter',
+    1 => 'tw',
+    2 => 'folder-contains-twitter',
+];
+$GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-tw'] = 'folder-contains-twitter';
---
+++ packages/xima_twitter_client/ext_localconf.php
@@ -0,0 +1,3 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('@import "EXT:xima_twitter_client/Configuration/TSconfig/page.tsconfig"');
