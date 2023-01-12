6c12eca21c8796d3bd956d5d208c7c50aa7f8bb2
Maik Schneider <mas@xima.de>
2023-01-12 11:43:58 +0100

    feat: add template for twitter

--- packages/xima_twitter_client/Configuration/Icons.php
+++ packages/xima_twitter_client/Configuration/Icons.php
@@ -5,4 +5,8 @@ return [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         'source' => 'EXT:xima_twitter_client/Resources/Public/Icons/icon-folder-twitter.svg',
     ],
+    'twitter' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:xima_twitter_client/Resources/Public/Icons/Extension.svg',
+    ]
 ];
