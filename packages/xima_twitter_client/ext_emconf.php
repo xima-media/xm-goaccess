18f62a9bc61c4db1fd9ae891113b3a7a0d765742
Maik Schneider <mas@xima.de>
2023-01-16 09:40:32 +0100

    feat: add ext_emconf for twitter extension

---
+++ packages/xima_twitter_client/ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Twitter client',
+    'description' => 'Download and display tweets from Twitter',
+    'category' => 'plugin',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-11.99.*',
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Xima\\XimaTwitterClient\\' => 'Classes',
+        ],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'author' => 'Maik Schneider',
+    'author_email' => 'maik.schneider@xima.de',
+    'author_company' => 'XIMA Media GmbH',
+    'version' => '1.0.0',
+];
