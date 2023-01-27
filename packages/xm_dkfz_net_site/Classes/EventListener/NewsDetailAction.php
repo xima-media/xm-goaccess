7916badcda1a897e2ca0f515b4e008e2c1538264
Maik Schneider <mas@xima.de>
2023-01-27 15:38:41 +0100

    chore: phpfixer

--- packages/xm_dkfz_net_site/Classes/EventListener/NewsDetailAction.php
+++ packages/xm_dkfz_net_site/Classes/EventListener/NewsDetailAction.php
@@ -2,14 +2,11 @@
 
 namespace Xima\XmDkfzNetSite\EventListener;
 
-use Blueways\BwGuild\Event\InitializeUserUpdateEvent;
 use GeorgRinger\News\Event\NewsDetailActionEvent;
-use TYPO3\CMS\Extbase\Property\TypeConverter\DateTimeConverter;
 use Xima\XmDkfzNetSite\Domain\Repository\NewsRepository;
 
 class NewsDetailAction
 {
-
     public function __construct(protected NewsRepository $newsRepository)
     {
     }
@@ -21,7 +18,7 @@ class NewsDetailAction
 
         if ($uid) {
             $news = $this->newsRepository->findByUid($uid);
-            $values['color'] = $news->getTxXmdkfznetsiteColor();
+            $values['color'] = $news?->getTxXmdkfznetsiteColor() ?? '';
             $event->setAssignedValues($values);
         }
     }
