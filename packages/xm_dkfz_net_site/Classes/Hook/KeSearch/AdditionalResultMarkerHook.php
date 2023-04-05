368190a4d2a0271ad2828263284655603bc29162
Maik Schneider <schneider.maik@me.com>
2023-04-05 13:43:39 +0200

    feat: calculate label of search result badge in ke_search hook

--- packages/xm_dkfz_net_site/Classes/Hook/KeSearch/AdditionalResultMarkerHook.php
+++ packages/xm_dkfz_net_site/Classes/Hook/KeSearch/AdditionalResultMarkerHook.php
@@ -18,6 +18,8 @@ class AdditionalResultMarkerHook
         if ($row['type'] === 'page') {
             $tempMarkerArray['rootline'] = $this->getRootlineArrayForPage((int)$tempMarkerArray['orig_row']['uid']);
         }
+
+        $tempMarkerArray['badgeLabel'] = $this->getBadgeLabelForRow($row);
     }
 
     protected function getRootlineArrayForPage(int $uid): array
@@ -25,5 +27,27 @@ class AdditionalResultMarkerHook
         $rootline = GeneralUtility::makeInstance(RootlineUtility::class, $uid)->get();
 
         return array_reverse($rootline, true);
+    }
+
+    protected function getBadgeLabelForRow(array $row): string
+    {
+        $badgeLabel = '';
+        if ($row['type'] === 'news') {
+            $badgeLabel = 'news';
+        }
+        if (str_starts_with($row['type'], 'file:')) {
+            $badgeLabel = substr($row['type'], 5);
+        }
+        if ($row['type'] === 'xmkesearchremote') {
+            $tags = GeneralUtility::trimExplode(',', $row['tags'], true);
+            if (in_array('#wiki#', $tags)) {
+                $badgeLabel = 'WIKI ITCF';
+            }
+            if (in_array('#dkfzwebsite#', $tags)) {
+                $badgeLabel = 'dkfz.de';
+            }
+        }
+
+        return $badgeLabel;
     }
 }
