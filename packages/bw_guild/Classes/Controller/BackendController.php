a5e1417ef82723d36a2f9663571bf113ecb06ccc
Maik Schneider <mas@xima.de>
2023-04-25 08:26:54 +0200

    chore: cleanup BackendController and templates

--- packages/bw_guild/Classes/Controller/BackendController.php
+++ packages/bw_guild/Classes/Controller/BackendController.php
@@ -5,49 +5,36 @@ namespace Blueways\BwGuild\Controller;
 use Blueways\BwGuild\Domain\Repository\OfferRepository;
 use Blueways\BwGuild\Domain\Repository\UserRepository;
 use Psr\Http\Message\ResponseInterface;
-use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
-use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Page\PageRenderer;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
-use TYPO3\CMS\Extbase\Domain\Repository\FrontendUserGroupRepository;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 final class BackendController extends ActionController
 {
     public function __construct(
         protected UserRepository $userRepository,
-        protected FrontendUserGroupRepository $usergroupRepository,
         protected OfferRepository $offerRepository,
-        protected readonly ModuleTemplateFactory $moduleTemplateFactory,
-        protected readonly IconFactory $iconFactory,
+        protected PageRenderer $pageRenderer
     ) {
     }
 
     public function indexAction(): ResponseInterface
     {
-        $typoScript = $this->configurationManager->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK);
-        $startView = $typoScript['settings']['startView'] ?? '';
+        $this->pageRenderer->addInlineLanguageLabelFile('EXT:core/Resources/Private/Language/locallang_mod_web_list.xlf');
 
         $users = $this->userRepository->findAll();
         $this->view->assign('users', $users);
 
-        $moduleTemplate = $this->moduleTemplateFactory->create($this->request);
-        $moduleTemplate->setContent($this->view->render());
-        return $this->htmlResponse($moduleTemplate->renderContent());
+        return $this->htmlResponse();
     }
 
     public function offerAction(): ResponseInterface
     {
+        $this->pageRenderer->loadRequireJsModule('TYPO3/CMS/BwGuild/BackendOffer');
+        $this->pageRenderer->addInlineLanguageLabelFile('EXT:core/Resources/Private/Language/locallang_mod_web_list.xlf');
+
         $offers = $this->offerRepository->findAll();
-        $offerGroups = $this->offerRepository->getGroupedOffers();
-
-        $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
-        $pageRenderer->loadRequireJsModule('TYPO3/CMS/BwGuild/BackendOffer');
-        $pageRenderer->addInlineLanguageLabelFile('EXT:core/Resources/Private/Language/locallang_mod_web_list.xlf');
-
         $this->view->assign('offers', $offers);
-        $this->view->assign('offerGroups', $offerGroups);
+
         return $this->htmlResponse();
     }
 }
